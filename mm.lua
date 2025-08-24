cbbd11244bbd7ba8ab937bbfdd8b9eafdf7c73a4
abc-xyz-nmh <nghhai0208@gmail.com>
2025-08-24 23:04:49 +0700

    Update mm.lua

--- mm.lua
+++ mm.lua
@@ -37,7 +37,7 @@ getgenv().gagConfig = {
 
     -- EQUIP_PETS Priority (Left -> Right)
     EQUIP_PETS = { ["Gorilla Chef"] = 5, ["Sunny-Side Chicken"] = 5, ["Capybara"] = 1, ["Starfish"] = 8},
-    USE_PETS_FOR_UPGRADE_SLOT = { "Starfish", "Capybara" },
+    USE_PETS_FOR_UPGRADE_SLOT = { "Starfish" },
 
     BUY_GEAR_SHOP = { "Master Sprinkler", "Godly Sprinkler", "Advanced Sprinkler", "Basic Sprinkler" },
     USE_SPRINKLER = { "Basic Sprinkler", "Master Sprinkler", "Godly Sprinkler", "Advanced Sprinkler" },
