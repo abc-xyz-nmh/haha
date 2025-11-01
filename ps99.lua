1924c3db67d8845b10320f8ba03666ea29d1a91d
abc-xyz-nmh <nghhai0208@gmail.com>
2025-11-01 18:12:20 +0700

    Update ps99.lua

--- ps99.lua
+++ ps99.lua
@@ -1,19 +1,32 @@
-script_key="VNtnDQmHSDNJbelAlhBhWjSzFKIzvXYg";
 getgenv().Config = {
     DPS_THRESHOLDS = {
-        HOUSE1 = { '>0' },
-        HOUSE2 = { '>1000' },
-        HOUSE3 = { '6000~14000', '>25000' },
-        HOUSE4 = { '14000~20000' },
-        HOUSE5 = { '>24000' },
+        HOUSE1 = { '>0' }, -- mua theo dps , buy by dps
+        HOUSE2 = { '>1000' }, 
+        HOUSE3 = { '6000~14000', '>25000' }, 
+        HOUSE4 = { '14000~20000' }, 
+        HOUSE5 = { '>24000' }, 
     },
     plant = {
-        PET_SLOTS = { 1, 2, 3, 4, 5, 6, 7 , 8, 9}, -- slot dành cho pet
-        EGG_SLOTS = { 10 }, -- slot dành cho trứng
+        PET_SLOTS = { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, -- slot đặt pet, put pet
+        EGG_SLOTS = { 10 }, -- slot dành cho trứng -- slot đặt trứng , put egg
     },
-    Usernames = { 'NguyenVanThuan47' }, -- Danh sách người nhận
-    PetSendInterval = 10, -- Thời gian quét và gửi lại (giây)
-    MinDiamonds = 50000000, -- Gửi khi kim cương > số này
-    Receivers = { 'NguyenVanThuan47' }, -- Danh sách người nhận
+    -- Gửi PET Titanic/Huge
+    SEND_PET = {
+        Usernames = { 'NAME1', 'NAME2', 'NAME3' }, --gửi cho nhiều usename,send multi usename
+        PetSendInterval = 20,
+    },
+    -- Gửi DIAMONDS
+    SEND_DIAMONDS = {
+        Usernames = { 'NAME1', 'NAME2', 'NAME3' },--gửi cho nhiều usename,send multi usename
+        MinDiamonds = 1000000,
+    },
+    -- Gửi ITEM
+    SEND_ITEM = {
+        Usernames = { 'NAME1', 'NAME2', 'NAME3' },--gửi cho nhiều usename,send multi usename
+        ['Halloween Lucky Egg'] = { amount = 10 },
+        ['Golden Apple'] = { amount = 'all' },
+        ['Spectral Potion'] = { amount = 3 },
+    },
 }
+script_key="VNtnDQmHSDNJbelAlhBhWjSzFKIzvXYg";
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/650444755c5495d80abdc753187316ac.lua"))()
