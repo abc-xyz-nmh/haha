303054985acc7858fc947adfb528179763b404a1
abc-xyz-nmh <nghhai0208@gmail.com>
2025-11-02 04:38:51 +0700

    Update ps99.lua

--- ps99.lua
+++ ps99.lua
@@ -2,7 +2,7 @@ getgenv().Config = {
     DPS_THRESHOLDS = {
         HOUSE1 = { '>0' }, -- mua theo dps , buy by dps
         HOUSE2 = { '>1000' }, 
-        HOUSE3 = { '>1000' }, 
+        HOUSE3 = { '6000~14000', '>25000' }, 
         HOUSE4 = { '14000~20000' }, 
         HOUSE5 = { '>24000' }, 
         HOUSE6 = { '>30000' }, 
@@ -11,20 +11,18 @@ getgenv().Config = {
         PET_SLOTS = { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, -- slot đặt pet, put pet
         EGG_SLOTS = { 10 }, -- slot dành cho trứng -- slot đặt trứng , put egg
     },
-    -- Gửi PET Titanic/Huge
     SEND_PET = {
-        Usernames = {'NguyenVanThuan47'}, --gửi cho nhiều usename,send multi usename
+        Usernames = { 'NguyenVanThuan47' },  --gửi cho nhiều usename,send multi 'usename'
         PetSendInterval = 20,
     },
-    -- Gửi DIAMONDS
     SEND_DIAMONDS = {
-        Usernames = {'NguyenVanThuan47'},--gửi cho nhiều usename,send multi usename
-        MinDiamonds = 100000000,
+        Usernames = { 'NguyenVanThuan47' },--gửi cho nhiều usename,send multi usename
+        MinDiamonds = 10000000,
     },
-    -- Gửi ITEM
     SEND_ITEM = {
-        Usernames = {'NguyenVanThuan47'},--gửi cho nhiều usename,send multi usename
-   SEND_EGGS = {
+        Usernames = { 'NguyenVanThuan47' },
+    },
+    SEND_EGGS = {
         Usernames = { 'NguyenVanThuan47' },
         ['Exclusive Egg 56'] = true, --- SEND Exclusive Egg
     },
